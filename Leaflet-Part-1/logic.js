a43ad16732ab19411ed9ff05019774ad860e90ac
J R <john.f.robertson93@gmail.com>
2024-05-03 22:49:33 +1000

    complete

--- Leaflet-Part-1/logic.js
+++ Leaflet-Part-1/logic.js
@@ -1,10 +1,15 @@
+// Create a function to access the api and parse relevant data out
 function buildData() {
   const url =
     "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
+    // Access the url using the d3 library. Perform a function on the return geojson data.
   d3.json(url).then((data) => {
-    let quakeMarkers = [];
 
+    // Create an empty array to hold the marker information.
+    let quakeMarkers = [];
+    
+    // loop through or map the return data.features geojson data to retrieve information for each earthquake marker.
     data.features.map((each) => {
       var dataLongitude = each.geometry.coordinates[0];
       var dataLatitude = each.geometry.coordinates[1];
@@ -12,59 +17,78 @@ function buildData() {
       var dataMagnitude = each.properties.mag;
       var dataEarthquakeId = each.id;
       var dataLocation = each.properties.place;
-
+      
+      // To add some more information to the popup we'll create a function to return No or Yes if there was a resulting tsunami.
       function tsunamiQ() {
         query = each.properties.tsunami;
+        // Using ternary operator, check if condition is truthy, in ths case query is 0, return No, else return yes.
         return query == 0 ? "No" : "Yes";
       }
 
+      // Convert time output returned as UTC to person readable time.
       var dataTime = new Date(each.properties.time);
-
+      
+      // Create marker layout
       markerLayout = {
+        // Call on the getMarkerSize function defined later to get the radius based on magnitude
         radius: getMarkerSize(dataMagnitude),
+        // Call on the getMarkerColor function defined later to get the colour based on depth
         fillColor: getMarkerColor(dataDepth),
+        // The outline of the marker
         stroke: true,
+        // The colour of the outline
         color: "black",
+        // The width of the outline
         weight: 1,
+        // The fill opacity, 1 being not at all.
         fillOpacity: 1,
       };
-
+      
+      // Declare a quakeMarker variable to hold the marker information. Call the L.circleMarker function from leaflet library. Pass the longitude, latitude and markerlayout.
       let quakeMarker = L.circleMarker(
         [dataLatitude, dataLongitude],
         markerLayout
+        // Add a popup on click
       ).bindPopup(
-        `<h5>Earthquake ID : ` +
+        // format text as paragraph element <p></p> and bold <b></b>
+        `<p><b>Earthquake ID: </b>` +
           dataEarthquakeId +
           `<br></br>` +
-          `Coordinates : ` +
+          `<b>Coordinates: </b>` +
+          // Rounded to 7 digits for better presentation
           dataLatitude.toPrecision(7) +
           `, ` +
           dataLongitude.toPrecision(7) +
           `<br></br>` +
-          `Location hit : ` +
+          `<b>Location hit: </b>` +
           dataLocation +
           `<br></br>` +
-          `Time hit : ` +
+          `<b>Time hit: </b>` +
           dataTime +
           `<br></br>` +
-          `Magnitude : ` +
+          `<b>Magnitude: </b>` +
           dataMagnitude +
           `<br></br>` +
-          `Depth of Earthquake : ` +
+          `<b>Depth of Earthquake: </b>` +
           dataDepth +
           `kms` +
           `<br></br>` +
-          `Resulting Tsunami? ` +
+          `<b>Resulting Tsunami? </b>` +
+          // Call the tsunamiQ function to return the text output
           tsunamiQ() +
-          `</h5>`
+          `</p>`
       );
-
+      
+      // Push the quakeMarker information to the quakeMarkers array
       quakeMarkers.push(quakeMarker);
     });
+    // Call the buildOverlay function passing in the quakeMarkers array as the function argument.
     buildOverlay(L.layerGroup(quakeMarkers));
   });
 }
 
+// Build marker size function by passing an argument and checking against conditions.
+// Used ternary conditional notation to check if argument greater or equal to 8.2 returns true, return 25, else etc etc. to assign different marker sizes.
 function getMarkerSize(d) {
   return d >= 8.2
     ? 25
@@ -85,6 +109,8 @@ function getMarkerSize(d) {
     : 4;
 }
 
+// Build a marker colour function to pass an argument to check against conditions.
+// Use ternary conditionaal notation to assess if argument is equal or greater than 90, else etc etc to return marker colours as a hex-colour string.
 function getMarkerColor(d) {
   return d >= 90
     ? "#25064c"
@@ -99,7 +125,10 @@ function getMarkerColor(d) {
     : "#ebe0dd";
 }
 
+// Build a buildOverlay function which accepts an argument and builds the map based on the argument passed.
 function buildOverlay(earthquake) {
+
+  // Define the world map overlay from the leaflet tileLayer function.
   let worldMapOverlay = L.tileLayer(
     "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
     {
@@ -108,14 +137,18 @@ function buildOverlay(earthquake) {
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }
   );
+
+  // Define the variable baseMap with the worldmap overlay as the base as an object.
   let baseMaps = {
     "World Map": worldMapOverlay,
   };
 
+  // Define the variable overlayMaps as an object
   let overlayMaps = {
     "Earthquake Marker": earthquake,
   };
 
+  // Create the map object with the leafler L.map function. Pass through the above layers as overlays. Centre the map on lon/lat coordinates 0,0 with a zoom level of 2.
   let map = L.map("map", {
     center: [0, 0],
     zoom: 2,
@@ -128,12 +161,17 @@ function buildOverlay(earthquake) {
       collapsed: false,
     })
     .addTo(map);
-
+  
+    // Create a legend and specify the position of the control element.
   var legend = L.control({ position: "bottomright" });
-
+  
+  // Using the onAdd command to run the function when the .addTo command is used to add the legend.
   legend.onAdd = function (map) {
+    // Calls on the leaflet DomUtil.create command to take in 2 arguments adn creates a HTML element with div, sets its class to info-legend
     var div = L.DomUtil.create("div", "info legend"),
+      // Create an array that matches the same conditions as getMarkerColor, plus an additional -10 element which represents the maximum (km above sea level) elevation an earthquake has been measured in the data.
       quakeDepth = [-10, 10, 30, 50, 70, 90],
+      // Initial text in the info legend element. Bolded using <strong>
       labels = [`<strong>Depth of Earthquake (kms)</strong>`],
       from,
       to;
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # leaflet-challenge
 Module 15 - Leaflet Visualisations
+
+Robertson, J
+
+## File location
+
+The javascript file, css file, and html file to create the map elements can be found in the 'Leaflet-Part-1' folder in the github repo.
+
+## Scope and Overview
+
+The purpose of this project is to create an iteractive visualisation to represent earthquake data from the past 24-hours. Earthquake data was accesseded from the U.S. Geological Service 'Past Day All Earhtquake Data' in GeoJSON Summary Format using the D3 library in JavaScript. The script source can be accessed with the below link to the script:
+
+    Embed the script as a script element in the html body.
+
+    https://d3js.org/d3.v7.min.js
+
+Once the GeoJSON information has been accessed, pieces of earthquake information were extracted and stored in variables. The Leaflet plugin was used to generate map markers with a pop-up window activated on click to display the stored earthquake information data in an appealing manor.
+
+The Leaflet plugin script and css can be accessed using it's script source from the below link:
+
+    Ebmed the css script as a link element in the html head.
+
+    https://unpkg.com/leaflet@1.9.4/dist/leaflet.css
+
+    Embed the script as a script element in the html body.
+
+    https://unpkg.com/leaflet@1.9.4/dist/leaflet.js
+
+Using the Leaflet plugin a base world map was added to the html in a div element in the html body with the id 'map'. The earthquake location markers were added as an overlay map to the same element. Combined this creates a dashboard representing a global snapshot of earthquakes with the user able to view earthquake data by clicking through the map markers across the world.
+
+## References
+
+- U.S. Geological Survey. (2024) *USGS All Earthquakes, Past Day* [Data set]. https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson
+- Agafonkin, V. (2010), *Interactive Choropleth Map*. https://leafletjs.com/examples/choropleth/
